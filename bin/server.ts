befa4b32a894c59e0109fc91a6d8973deb1d11da
charlie50503 <cate50503@gmail.com>
2022-06-26 17:46:45 +0900

    typescript init

--- bin/server.ts
+++ bin/server.ts
@@ -3,10 +3,10 @@
 /**
  * Module dependencies.
  */
-
-var app = require('../app');
-var debug = require('debug')('metawall-backend:server');
-var http = require('http');
+import app from "../app"
+import debug from "debug"
+debug('todolist-mongodb-typescript:server')
+import http from "http"
 
 /**
  * Get port from environment and store in Express.
@@ -33,7 +33,7 @@ server.on('listening', onListening);
  * Normalize a port into a number, string, or false.
  */
 
-function normalizePort(val) {
+function normalizePort(val: string) {
   var port = parseInt(val, 10);
 
   if (isNaN(port)) {
@@ -53,7 +53,7 @@ function normalizePort(val) {
  * Event listener for HTTP server "error" event.
  */
 
-function onError(error) {
+function onError(error: any) {
   if (error.syscall !== 'listen') {
     throw error;
   }
@@ -85,6 +85,6 @@ function onListening() {
   var addr = server.address();
   var bind = typeof addr === 'string'
     ? 'pipe ' + addr
-    : 'port ' + addr.port;
+    : 'port ' + addr?.port;
   debug('Listening on ' + bind);
 }
