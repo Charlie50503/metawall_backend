0d054df40c4b4599ef2ad4fdb1d13972e0d9f4ac
charlie50503 <cate50503@gmail.com>
2022-06-26 21:27:27 +0900

    create postModel

---
+++ models/interface/post.ts
@@ -0,0 +1,13 @@
+import { Schema, Types } from 'mongoose';
+export interface Post {
+  creator: Types.ObjectId,
+  content: string,
+  imgURL: string,
+  comments: Array<Types.ObjectId>,
+  isDelete: boolean,
+  likes: Array<Types.ObjectId>,
+  createAt: Date,
+  updateAt: Date
+
+}
+
