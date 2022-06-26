7020540e7f92d643f059561bf0185e8e03038a1d
charlie50503 <cate50503@gmail.com>
2022-06-26 21:55:05 +0900

    feat: add userModel

---
+++ models/interface/user.ts
@@ -0,0 +1,10 @@
+export interface UserModelDto {
+    nickName: string;
+    sex: string;
+    avatar: string;
+    email: string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+    isDeleted: boolean;
+}
