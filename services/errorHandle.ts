6e664a9249a31980ac74941c86746c7e215f6193
charlie50503 <cate50503@gmail.com>
2022-07-04 00:52:16 +0900

    feat: 添加errorHandle

---
+++ services/errorHandle.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import { customError } from "./errorHandle/interface";
+
+
+export class ErrorHandle {
+  public static handleErrorAsync = (func: any) => {
+    return function (req: express.Request, res: express.Response, next: express.NextFunction) {
+      func(req, res, next).catch(function (error: express.ErrorRequestHandler) {
+        return next(error)
+      })
+    }
+  }
+
+  public static resErrorDev = (err: customError | any, res: express.Response) => {
+    res.status(res.statusCode).json({
+      message: err.message,
+      error: err,
+      stack: err.stack
+    })
+  }
+
+  public static resErrorProd = (err: customError | any, res: express.Response) => {
+    if (err.isOperational) {
+      res.status(err.statusCode).json({
+        message: err.message
+      })
+    } else {
+      console.log("出現重大錯誤", err)
+      res.status(500).json({
+        status: "error",
+        message: "系統錯誤，請洽管理員"
+      })
+    }
+  }
+}
