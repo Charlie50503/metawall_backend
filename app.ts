012ae2dbf7a744993cce820c74f418263a05e202
charlie50503 <cate50503@gmail.com>
2022-07-04 01:42:17 +0900

    feat: 添加全域錯誤處理

--- app.ts
+++ app.ts
@@ -3,12 +3,21 @@ import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
-
+import { ErrorHandle } from "./services/errorHandle/errorHandle"
 import indexRouter from "./routes/index";
 import userRouter from "./routes/user";
 import postRouter from "./routes/post";
 
 const app = express();
+
+// 程式出現重大錯誤時
+process.on('uncaughtException', err => {
+  // 記錄錯誤下來，等到服務都處理完後，停掉該 process
+	console.error('Uncaught Exception！')
+	console.error(err);
+	process.exit(1);
+});
+
 import "./connections";
 // import { errorHandle} from "./services/errorHandle"
 // view engine setup
@@ -30,6 +39,21 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
+app.use(function (err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
+  err.statusCode = err.statusCode || 500
+  if (process.env.NODE_ENV === 'dev') {
+    return ErrorHandle.resErrorDev(err, res)
+  }
+
+  //production
+  if (err.name === 'ValidationError') {
+    err.name = '資料欄位未填寫正確，請重新輸入'
+    err.isOperational = true
+    return ErrorHandle.resErrorProd(err, res)
+  }
+  ErrorHandle.resErrorProd(err, res)
+})
+
 // error handler
 app.use(function (
   err: any,
@@ -46,4 +70,8 @@ app.use(function (
   res.render("error");
 });
 
+
+process.on('unhandledRejection',(error,promise)=>{
+  console.error('未捕捉到 rejection:',promise,'原因',error)
+})
 export default app;
