b41659504b7e7859bec8b273d3daa160faea85fa
charlie50503 <cate50503@gmail.com>
2022-07-19 23:45:37 +0900

    feat: 新增解讀Token userId的共通方法

--- services/jwt.ts
+++ services/jwt.ts
@@ -1,5 +1,7 @@
 import jwt from "jsonwebtoken"
 import { DotenvParseOutput } from "dotenv"
+import express from "express";
+import { ErrorHandle } from "./errorHandle/errorHandle";
 export class JWT {
   public static generateJwtToken(id: string): Promise<string> {
     console.log("id",id);
@@ -12,5 +14,40 @@ export class JWT {
       if (!token) return reject("token 生成錯誤")
       return resolve(token)
     })
+  }
+
+  public static async decodeTokenGetId(
+    req: express.Request, res: express.Response, next: express.NextFunction
+  ) {
+    let token = ""
+
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer')
+    ) {
+      token = req.headers.authorization.replace('Bearer ', '') as string;
+    }
+
+    if (!token) {
+      return next(ErrorHandle.appError("401", "沒有登入", next))
+    }
+
+    let decoded:any;
+    try {
+      decoded = await new Promise<string | jwt.JwtPayload | undefined | jwt.VerifyErrors>((resolve, reject) => {
+        const { JWT_SECRET } = process.env as DotenvParseOutput
+
+        const result = jwt.verify(token, JWT_SECRET) as { id: string };
+        if(!result?.id) { reject("token 錯誤") }
+        resolve(result)
+      })
+    } catch (error: jwt.VerifyErrors | any) {
+      next(ErrorHandle.appError("401", "token 不正確", next))
+    }
+
+    return decoded['id'];
+
+    // req.user = currentUser
+
   }
 }
