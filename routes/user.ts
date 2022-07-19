bea12713fa0612230b80e2a84ab9f668c0f48f72
charlie50503 <cate50503@gmail.com>
2022-07-19 23:42:23 +0900

    feat: 取得個人資料API 添加Token驗證middleware

--- routes/user.ts
+++ routes/user.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import UsersController from "../controllers/userController";
 import { RequestParams } from "../middleware";
+import Auth from "../middleware/auth";
 import { ErrorHandle } from "../services/errorHandle/errorHandle";
 var router = express.Router();
 
 /* GET users listing. */
-router.get("/profile/:id", ErrorHandle.handleErrorAsync(UsersController.getProfile));
+router.get("/profile/:id", Auth.checkToken,ErrorHandle.handleErrorAsync(UsersController.getProfile));
 router.post("/sign-in",RequestParams.signIn, ErrorHandle.handleErrorAsync(UsersController.postSignIn));
 router.post("/sign-up",RequestParams.signUp, ErrorHandle.handleErrorAsync(UsersController.postSignUp));
 router.get("/check-is-user",Auth.checkToken,(req:express.Request,res: express.Response )=>{
